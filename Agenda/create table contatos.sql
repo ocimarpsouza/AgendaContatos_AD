e09474aa6dfea791defda3b9a45d195d1739e553
ocimarpsouza <ocimarpsouza@gmail.com>
2017-09-30 14:32:10 -0300

    Add files via upload

---
+++ Agenda/create table contatos.sql
@@ -0,0 +1,17 @@
+﻿Create database Agenda
+go
+use Agenda
+go
+create table Contatos(
+id int identity primary key,
+nome varchar(100),
+email varchar(100),
+foneCel varchar(20),
+foneRes varchar(20),
+foneCom varchar(20),
+ender varchar(100),
+bairro varchar(50),
+cidade varchar(50),
+uf char(2),
+cep varchar(9)
+)
